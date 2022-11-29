c53b58598f7534c565e24f5a55fe7dc6f030f8c9
Nancy Yoder <nayo4039@colorado.edu>
2022-11-29 09:35:08 -0700

    hghghhg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import Text from './Text.js';
 // import Page from './Page.js';
-// import Page1 from './Page1.js';
-// import Page2 from './Page2.js';
+import Page1 from './Page1.js';
+import Page2 from './Page2.js';
 import Page3 from './Page3.js';
 import Page4 from './Page4.js';
 import './App.css';
@@ -11,8 +11,8 @@ function App() {
   return (
     // should each page be a class (i.e. <Page1 />) or should they all be 1 class (i.e. <Page content = "" />)
     <div className="App">
-      {/* <Page1 color='black' /> */}
-      {/* <Page2 color='black' /> */}
+      {/* <Page1 color='black' />
+      <Page2 color='black' /> */}
       <Page3 color='#B56DAD' />
       <Page4 color='#B56DAD' />
     </div>
