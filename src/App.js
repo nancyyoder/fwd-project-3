4eaedc13a0b1a7c690710f55c3d2b0117e55f295
Jordan Evans <77519478+jordanaevanz@users.noreply.github.com>
2022-12-01 09:15:23 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 // import Text from './Text.js';
 // import Page from './Page.js';
+
+// import Page3 from './Page3.js';
+// import Page4 from './Page4.js';
+import './App.css';
+import { 
+  BrowserRouter,
+  Switch,
+  Route,
+  // Link
+   } from "react-router-dom";
+
+
 import Page1 from './Page1.js';
 import Page2 from './Page2.js';
-import Page3 from './Page3.js';
-import Page4 from './Page4.js';
-import './App.css';
-
 
 function App() {
-  return (
-    // should each page be a class (i.e. <Page1 />) or should they all be 1 class (i.e. <Page content = "" />)
-    <div className="App">
-      {/* <Page1 color='black' />
-      <Page2 color='black' /> */}
-      <Page3 color='#B56DAD' />
-      <Page4 color='#B56DAD' />
-    </div>
+  return(
+    <BrowserRouter>
+            <div className="container">
+                <Switch>
+                    <Route path="/Page1" component={Page1} />
+                    <Route path="/Page2" component={Page2} />
+                </Switch>
+            </div>
+       </BrowserRouter>
   );
 }
 
